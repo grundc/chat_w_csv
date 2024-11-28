d87a8ddcedfa2da90d04686dc9427ce8ada3ed23
Christoph Grund <christoph.grund@web.de>
2024-11-28 23:04:11 +0100

    Added some new features for cache history and prompt history

--- app.py
+++ app.py
@@ -9,24 +9,49 @@ from pandasai import Agent
 from pandasai.llm import OpenAI
 import pandas as pd
 from pandasai.responses.streamlit_response import StreamlitResponse
+import pandasai as pai
+
+# **********************************************
+# Setup Session prompt history
+if "prompt_history" not in st.session_state:
+        st.session_state.prompt_history = []
+# **********************************************
 
 
-# st.title("Chat with your CSV")
+
 st.sidebar.title("PandasAI")
-st.sidebar.subheader("Configuration")
+st.sidebar.info("*Configuration*")
 
 OpenAI_API_KEY = st.sidebar.text_input("OpenAI API Key", type="password")
 explain = st.sidebar.toggle("Explain", False, key="explain")
 questions = st.sidebar.toggle("Questions", False, key="questions")
 
-st.sidebar.subheader("Data")
+st.sidebar.info("*Data*")
 csv_file = st.sidebar.file_uploader("Upload a CSV file", type=["csv"])
 
-# csv_file = st.file_uploader("Upload a CSV file", type=["csv"])
 
+
+with st.sidebar:
+    st.info("*Prompt History*")
+    with st.container(height=200, border=True):
+        for item in st.session_state.prompt_history:
+            if st.button(f"'{item[:20]}...'", help=f"{item}", use_container_width=True):
+                st.session_state.prompt = item  
+                
+if st.sidebar.button("Clear Cache", type="primary", use_container_width=True):
+    pai.clear_cache()
+    st.session_state.prompt_history = []
+    st.session_state.prompt = ''
+# *********************************************************************************
+
+st.title("Chat with your CSV")
 if csv_file:
-
-    df = pd.read_csv(csv_file)
+    try:
+        df = pd.read_csv(csv_file)
+    except Exception as e:
+        st.error(f"Error reading CSV file: {e}")
+        st.stop()
+        
     # st.write(df.head(5))
     columns = df.columns
     column_selection = st.multiselect("Column Selector", columns, list(columns))
@@ -34,8 +59,10 @@ if csv_file:
     if column_selection:
         df = df[column_selection]
         st.write(df.head(5))
-
-    prompt = st.text_area("Prompt")
+    
+    
+    st.subheader("Prompt")
+    prompt = st.text_area("", value=st.session_state.prompt)
 
     if st.button("Process"):
 
@@ -65,8 +92,14 @@ if csv_file:
                     st.info("Clarification Questions")
                     questions = agent.clarification_questions(prompt)
                     st.markdown("\n".join([f"- {question}" for question in questions]))
+
+            if prompt in st.session_state.prompt_history:
+                st.session_state.prompt_history.remove(prompt)
+            st.session_state.prompt_history.append(prompt)
                 
         else:
             st.error("Please provide a prompt")
+        
+    
                        
        
